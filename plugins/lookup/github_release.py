5896e33cd9838c76b117552236ec5ddc37c6cec5
Daniel Vidal de la Rubia <dvrubia@gmail.com>
2023-06-15 15:36:36 +0200

    Use the license corrrectly

--- plugins/lookup/github_release.py
+++ plugins/lookup/github_release.py
@@ -1,7 +1,7 @@
 # Copyright © 2023 Daniel Vidal de la Rubia <https://github.com/Vidimensional>
 # This work is free. You can redistribute it and/or modify it under the
 # terms of the Do What The Fuck You Want To Public License, Version 2,
-# as published by Sam Hocevar. See the COPYING file for more details.
+# as published by Sam Hocevar. See the LICENSE file for more details.
 
 __metaclass__ = type
 
